3558bda09ad18e9c168ea9ee7832346dc11ec01b
psittacine <psittacine714@gmail.com>
2018-09-04 22:49:15 -0500

    Test for menu is hidden by default

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -61,6 +61,10 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden by default', function() {
+            const body = $('body');
+            expect(body.hasClass('menu-hidden')).toBe(true);
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
